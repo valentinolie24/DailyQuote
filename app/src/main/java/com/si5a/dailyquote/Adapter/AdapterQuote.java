9ce0af2bb5c9ab5b5437d56ac2a182080fa735d5
valentinolie24 <valenlie2409@gmail.com>
2022-12-05 14:32:45 +0700

    update 4

---
+++ app/src/main/java/com/si5a/dailyquote/Adapter/AdapterQuote.java
@@ -0,0 +1,56 @@
+package com.si5a.dailyquote.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.si5a.dailyquote.Model.QuoteModel;
+import com.si5a.dailyquote.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterQuote extends RecyclerView.Adapter<AdapterQuote.ViewHolderQuote>{
+    private List<QuoteModel> listQuote = new ArrayList<>();
+    private Context ctx;
+
+    public AdapterQuote(List<QuoteModel> listQuote, Context ctx) {
+        this.listQuote = listQuote;
+        this.ctx = ctx;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderQuote onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(ctx).inflate(R.layout.item_quote, parent,  false);
+        return new ViewHolderQuote(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderQuote holder, int position) {
+        QuoteModel QM = listQuote.get(position);
+        holder.tvText.setText(QM.getText());
+        holder.tvAuthor.setText(QM.getAuthor());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listQuote.size();
+    }
+
+    public class ViewHolderQuote extends RecyclerView.ViewHolder {
+        TextView tvText, tvAuthor;
+
+        public ViewHolderQuote(@NonNull View itemView) {
+            super(itemView);
+            tvText = itemView.findViewById(R.id.tv_text);
+            tvAuthor = itemView.findViewById(R.id.tv_author);
+        }
+    }
+}
